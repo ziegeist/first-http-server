c0867b242b6e25d9ad358b270e092fab97094009
ziegeist <marc.chang@gmail.com>
2016-12-30 13:10:48 +0800

    add dispatcher with GET and POST

--- myFirstHTTPServer.js
+++ myFirstHTTPServer.js
@@ -1,13 +1,38 @@
 //Lets require/import the HTTP module
 var http = require('http');
+var dispatcher = require('httpdispatcher');
 
 //Lets define a port we want to listen to
 const PORT=8080;
 
 //We need a function which handles requests and send repsonse
+//adding dispatcher
 function handleRequest(request, repsonse){
-	repsonse.end('It Works!! Path Hit: ' + request.url);
+	//repsonse.end('It Works!! Path Hit: ' + request.url);
+	try {
+		//log the request on console
+		console.log(request.url);
+		//Dispatch
+		dispatcher.dispatch(request, repsonse);
+	} catch(err) {
+		console.log(err);
+	}
 }
+
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('resources');
+
+//A sample GET request
+dispatcher.onGet("/page1", function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Page One');
+});
+
+//A sample POST request
+dispatcher.onPost("/post1", function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Got Post Data');
+});
 
 //Create a server
 var server = http.createServer(handleRequest);
